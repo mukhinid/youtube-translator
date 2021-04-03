08fbc8e7f7bcc7ca9fcd06da9efac9c83f46a460
Igor Mukhin <mukhinid@outlook.com>
2021-04-03 03:22:26 +0300

    Implemented language auto-detection for russian youtube interface.

--- src/content.ts
+++ src/content.ts
@@ -1,39 +1,49 @@
+import { getCode } from './language';
 import { TranslationService } from './translation.service';
-
-let isInjected = false;
-
-const interval = setInterval(() => {
-  if (!isInjected) {
-    console.debug('Not injected!');
-    isInjected = injectButton();
-  } else {
-    console.debug('Injected!');
-    clearInterval(interval);
-  }
-}, 3000);
-
-function injectButton(): boolean {
-  const popupHeader = document.querySelector('#language-name-row .metadata-editor-translated .language-header');
-  if (popupHeader) {
-    const button = document.createElement('button');
-    button.textContent = '🔁 Translate';
-    button.addEventListener('click', translate);
-    popupHeader.appendChild(button);
-    return true;
-  }
-  return false;
-}
 
 const translationService = new TranslationService();
 
-function translate() {
+setInterval(injectButtonToEachPopup, 3000);
+
+function injectButtonToEachPopup(): void {
+  const popups = document.querySelectorAll('#metadata-editor');
+  popups.forEach(popup => { injectButton(popup); });
+}
+
+function injectButton(popup: Element): void {
+  const fromHeader = popup.querySelector('#language-name-row .metadata-editor-original .language-header');
+  const toHeader = popup.querySelector('#language-name-row .metadata-editor-translated .language-header');
+  if (fromHeader && toHeader) {
+    if (toHeader.querySelector('.youtube-translator-button')) {
+      console.log('Button is already set!');
+      return;
+    }
+
+    const from = fromHeader.textContent;
+    const to = toHeader.textContent;
+
+    if (from && to) {
+      const button = document.createElement('button');
+      button.classList.add('youtube-translator-button');
+      button.textContent = '🔁 Translate';
+      button.addEventListener('click', () => {
+        const fromCode = getCode(from);
+        const toCode = getCode(to);
+        translate(fromCode, toCode);
+      });
+      toHeader.appendChild(button);
+    }
+  }
+}
+
+function translate(from: string, to: string) {
   const originalNodes = document.querySelectorAll('#scrollable-content-container .metadata-editor-original textarea');
   const translatedNodes = document.querySelectorAll('#scrollable-content-container .metadata-editor-translated textarea');
   originalNodes.forEach((node, i) => {
     const textArea = node as HTMLTextAreaElement;
     const content = textArea.value;
     if (content) {
-      translationService.translate(content, 'ru')
+      translationService.translate(content, from, to)
         .then(translation => {
           const translatedTextArea = translatedNodes[i] as HTMLTextAreaElement;
           translatedTextArea.value = translation;
--- src/translation.service.ts
+++ src/translation.service.ts
@@ -2,10 +2,10 @@ import { setCORS } from 'google-translate-api-browser';
 import { TranslationResult } from './translationResult';
 
 export class TranslationService {
-  private getTranslation = setCORS('https://cors-anywhere.herokuapp.com/');
+  private getTranslation = setCORS('https://youtube-translator-cors.herokuapp.com/');
 
-  public async translate(input: string, lang: string): Promise<string> {
-    const result = await this.getTranslation(input, { to: lang, tld: 'ru' });
+  public async translate(input: string, from: string, to: string): Promise<string> {
+    const result = await this.getTranslation(input, { from: from, to: to, tld: 'ru' });
     return (<TranslationResult>result).text;
   }
 }
