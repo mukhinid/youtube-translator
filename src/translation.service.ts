673919c84959c471d8c336ad899b392df77a8fe8
Mukhin Igor <mukhinid@outlook.com>
2021-08-10 00:42:03 +0300

    Added preservation of the source text when caching.

--- src/translation.service.ts
+++ src/translation.service.ts
@@ -4,21 +4,22 @@ import { TranslationResult } from './translationResult';
 export class TranslationService {
   private readonly getTranslation = setCORS('https://youtube-translator-cors.herokuapp.com/');
 
-  private readonly _cache = new Map<string, string[]>();
+  private readonly _cache = new Map<string, Map<string, string>>();
 
-  public async translate(input: string, from: string, to: string, index: number): Promise<string> {
+  public async translate(input: string, from: string, to: string): Promise<string> {
     const key = `${from}_${to}`;
     if (!this._cache.has(key)) {
-      this._cache.set(key, []);
+      this._cache.set(key, new Map<string, string>());
     }
     const langCache = this._cache.get(key);
-    if (index <= langCache!.length - 1) {
-      return langCache![index];
+    const inputCache = langCache?.get(input);
+    if (inputCache) {
+      return inputCache;
     }
 
     const result = await this.getTranslation(input, { from: from, to: to, tld: 'ru' });
     const resultText = (<TranslationResult>result).text;
-    langCache![index] = resultText;
+    langCache?.set(input, resultText);
     return resultText;
   }
 }
